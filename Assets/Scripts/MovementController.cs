5b43c0a89c7a92d6a24a3d076f7f6b7e2775fd0c
eriol1977 <eriol1977@hotmail.com>
2016-01-20 08:34:23 +0100

    Very first stuff

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour
+{
+	public float mouseSensitivity = 2.0f;
+
+	float rotLeftRight = 0;
+	public float leftRightRange = 60.0f;
+
+	float rotUpDown = 0;
+	public float upDownRange = 60.0f;
+
+	CharacterController cc;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		cc = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		rotLeftRight -= Input.GetAxis ("Mouse X") * mouseSensitivity;
+		rotLeftRight = Mathf.Clamp (rotLeftRight, -leftRightRange, leftRightRange);
+
+		rotUpDown -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		rotUpDown = Mathf.Clamp (rotUpDown, -upDownRange, upDownRange);
+
+		Camera.main.transform.localRotation = Quaternion.Euler (rotUpDown, -rotLeftRight, 0);
+	}
+}
---
+++ Assets/Scripts/ShootingController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingController : MonoBehaviour
+{
+	public GameObject ballPrefab;
+	public float ballImpulse = 10.0f;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Camera cam = Camera.main;
+		if (Input.GetButtonDown ("Fire1")) {
+			GameObject bullet = (GameObject)Instantiate (ballPrefab, cam.transform.position + cam.transform.forward, cam.transform.rotation);
+			bullet.GetComponent <Rigidbody> ().AddForce (cam.transform.forward * ballImpulse, ForceMode.Impulse);
+		}
+	}
+}
