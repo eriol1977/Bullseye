7350beb7b1080a81ec33a42e7279480c04807fcc
eriol1977 <eriol1977@hotmail.com>
2016-02-27 14:46:32 +0100

    trails scene

---
+++ Assets/Scripts/MoveOnTrails.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnTrails : MonoBehaviour
+{
+	public float speed = 10.0f;
+
+	void Update ()
+	{
+		Vector3 p = transform.position;
+		transform.position = new Vector3 (p.x, p.y, p.z + (speed * Time.deltaTime));
+	}
+}
---
+++ Assets/Scripts/Trails.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trails : MonoBehaviour
+{
+	public GameObject sleeperPrefab;
+
+	public float sleeperDistance = 0.5f;
+
+	void Start ()
+	{
+		GameObject sleeper;
+		float z = 0.0f;
+		while (z <= 100.0f) {
+			sleeper = (GameObject)Instantiate (sleeperPrefab, new Vector3 (0, 0, z), Quaternion.identity);
+			z += sleeperDistance;
+		}
+	}
+
+}
