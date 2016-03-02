b9bd6733a46284b7a85f4a80fc6f0eda94858102
eriol1977 <eriol1977@hotmail.com>
2016-03-02 08:26:57 +0100

    trail level implementation

---
+++ Assets/Scripts/Cart.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cart : MonoBehaviour
+{
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.transform.tag == "TrailStop") {
+			transform.parent.gameObject.GetComponent<MoveOnTrails> ().Bounce ();
+		}
+	}
+}
--- Assets/Scripts/MoveOnTrails.cs
+++ Assets/Scripts/MoveOnTrails.cs
@@ -5,9 +5,29 @@ public class MoveOnTrails : MonoBehaviour
 {
 	public float speed = 10.0f;
 
+	public float bouncingReduction = 0.2f;
+
+	private bool bouncing = false;
+
 	void Update ()
 	{
 		Vector3 p = transform.position;
 		transform.position = new Vector3 (p.x, p.y, p.z + (speed * Time.deltaTime));
+		if (bouncing) {
+			speed += bouncingReduction;
+			if (speed >= 0)
+				StopMotion ();
+		}
+	}
+
+	public void Bounce ()
+	{
+		speed = -speed;
+		bouncing = true;
+	}
+
+	public void StopMotion ()
+	{
+		speed = 0;
 	}
 }
